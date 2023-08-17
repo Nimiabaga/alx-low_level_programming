3a056e94311fd4f1be7dfe460803229674bbec54
nimiabaga <nimiabaga@yahoo.com>
2023-08-17 13:10:52 +0100

    debugging 0-main.c file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests function that points if integar is positive or nagative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	postive_nagative(i);
+
+	return (0);
+}
