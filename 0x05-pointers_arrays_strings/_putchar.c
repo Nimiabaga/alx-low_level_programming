bbf1d6491666ded4a7947102d3c2f9fc86e4c461
nimiabaga <nimiabaga@yahoo.com>
2023-08-21 19:22:53 +0100

    4-print_rev.c

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,17 +1,13 @@
-
-#include "main.h"
-/**
- * _puts - print string followed by new line
- *
- * @str: prints string
- */
-void _puts(char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
-}
+#include <unistd.h> 
+  
+ /** 
+  * _putchar - writes the character c to stdout 
+  * @c: The character to print 
+  * 
+  * Return: On success 1. 
+  * On error, -1 is returned, and errno is set appropriately. 
+  */ 
+ int _putchar(char c) 
+ { 
+         return (write(1, &c, 1)); 
+ }
