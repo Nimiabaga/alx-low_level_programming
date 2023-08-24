d3816df1ee28d0a50126f10765cd6e83acd0d85a
nimiabaga <nimiabaga@yahoo.com>
2023-08-24 11:57:48 +0100

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
 
 /**
  * _strcat - Concatenates two strings
- * *dest: The destination string
+ * @dest: The destination string
  * @src: The source string
  * Return: A pointer to the resulting string dest
  */
