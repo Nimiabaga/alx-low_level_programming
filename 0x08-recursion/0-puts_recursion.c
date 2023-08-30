e006bb737ff5c675234618391e70223bb374b041
nimiabaga <nimiabaga@yahoo.com>
2023-08-30 18:08:44 +0100

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,25 +1,20 @@
 #include "main.h"
-
-
 /**
- * _puts_recursion - recursive function to priny to screen
+ * _puts_recursion - recursive function to print to screen
  *
- * @s: pointer to the string 
+ * @s: pointer to the string
  *
- * Return: 0
+ * Return: nothing
  */
 void _puts_recursion(char *s)
 {
-        if (*s != '\0')
-        {
-
-                _putchar(*s);
-                _puts_recursion(s + 1);
-        }
-        if (*s == '\0')
-        {
-                _putchar('\n');
-
-        }
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
 }
-
