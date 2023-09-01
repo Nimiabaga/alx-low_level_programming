cff3e35124ab8cf0419a4dd3b04731265987ba6b
nimiabaga <nimiabaga@yahoo.com>
2023-09-01 19:38:19 +0100

    libmy.a, main.h

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -9,22 +9,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int a;
-	int b;
+	int len1, len2, i;
 
-	a = 0;
-	while (dest[a] != '\0')
-	{
-		a++;
-	}
-	b = 0;
-	while (src[b] != '\0')
+	for (len1 = 0; dest[len1] != '\0'; len1++)
 	{
 
-		dest[a] = src[b];
-		a++;
-		b++;
 	}
-	dest[a] = '\0';
+
+	for (len2 = 0; src[len2] != '\0'; len2++)
+	{
+
+	}
+	for (i = 0; i <= len2; i++)
+	{
+		dest[len1 + i] = src[i];
+	}
+
 	return (dest);
 }
