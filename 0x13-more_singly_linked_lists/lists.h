fb4da6bb8674942da47c84a37199482f8be63031
nimiabaga <nimiabaga@yahoo.com>
2023-09-25 20:02:16 +0100

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,15 +1,16 @@
-#ifndef _LISTS_H
-#define _LISTS_H_
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * struct listint_s - single referencial struct
- * for creating nodes of singly link list
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * @n: integer data
- * @next: single referencial pointer
- * pointer to the next node of list
+ * Description: singly linked list node structure
+ * for alx project
  */
 typedef struct listint_s
 {
@@ -17,8 +18,8 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-size_t listint_len(const listint_t *h);
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
@@ -34,3 +35,4 @@ size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
+
