186824bcf4952059fb91e8c5c3e4fb8d2d0cd23b
nimiabaga <nimiabaga@yahoo.com>
2023-09-28 14:41:44 +0100

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,16 +9,16 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
-	int i = 0;
+	int k = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[i] == '0' || b[i] == '1')
+	for (k = 0; b[k]; k++)
 	{
-		result <<= 1;
-		result += b[i] - '0';
-		i++;
+		if (b[k] < '0' || b[k] > '1')
+			return (0);
+		result = 2 * result + (b[k] - '0');
 	}
 	return (result);
 }
