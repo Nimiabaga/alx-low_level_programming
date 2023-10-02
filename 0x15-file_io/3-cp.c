2277c3a3b386afe1cb742e5e7111cbfa32f938e7
nimiabaga <nimiabaga@yahoo.com>
2023-10-03 00:23:27 +0100

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,47 +1,48 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
-#define ERR_NOCLOSE "Error: Can't close fd %d\n"
+#define ERR_NOREAD "Error: Can't read from from file %s\n"
 #define ERR_NOWRITE "Error: Can't write to %s\n"
-#define ERR_NOREAD "Error: Can't be read from file %s\n"
-#define USAGE "Usage: cp file_from_file_to\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
 #define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+#define USAGE "Usage: cp file_from file_to\n"
 
 /**
  * main - program
- * @ac: count argument
- * @av: vector arguemnt
- * Return: 1 on success 0 on failure
+ * @ac: count
+ * @av: vector
+ * Return: 1 on success
  */
 int main(int ac, char **av)
 {
-	int fd_from = 0;
-	int fd_to = 0;
+	int i = 0;
+	int j = 0;
+	ssize_t k;
 	char buffer[1024];
-	ssize_t i;
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
-	fd_from = open(av[1], O_RDONLY);
-	if (fd_from == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
-	if (fd_to == -1)
-		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
-
-	while ((i = read(fd_from, buffer, 1024)) > 0)
-		if (write(fd_to, buffer, i) != i)
-			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
-
+	i = open(av[1], O_RDONLY);
 	if (i == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+	j = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	if (j == -1)
+		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
-	fd_from = close(fd_from);
-	fd_to = close(fd_to);
-	if (fd_from)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd_from), exit(100);
-	if (fd_to)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd_from), exit(100);
+	while ((k = read(i, buffer, 1024)) > 0)
+		if (write(j, buffer, k) != k)
+			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+	if (k == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+	i = close(i);
+	j = close(j);
+	if (i)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, i), exit(100);
+	if (j)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, i), exit(100);
 
 	return (EXIT_SUCCESS);
+
+
+
 }
