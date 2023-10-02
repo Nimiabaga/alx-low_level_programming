cdee91c9bb66fc365402236ae4ca3757d8bc521e
nimiabaga <nimiabaga@yahoo.com>
2023-10-02 17:26:26 +0100

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,14 +6,13 @@
  */
 int _strlen(char *s)
 {
+	int j = 0;
 
-        int j = 0;
-        if (!s)
-                return(0);
-
-        while (*s++)
-                j++;
-        return(j);
+	if (!s)
+	return (0);
+	while (*s++)
+	j++;
+	return (j);
 }
 
 #include "main.h"
@@ -23,7 +22,7 @@ int _strlen(char *s)
  * @filename:  is the name of the file to create
  * @text_content: NULL terminated string to write to the file
  * Return: Returns: 1 on success, -1 on failure
- * (file can not be created, file can not be written, write “fails”, etc…)
+ * (file can not be created, file can not be written, write
  */
 int create_file(const char *filename, char *text_content)
 {
