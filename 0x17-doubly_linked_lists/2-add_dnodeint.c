a9f387af1177873ae7b328cc9817cddffcaaddfb
nimiabaga <nimiabaga@yahoo.com>
2023-12-07 17:22:29 +0100

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,30 +1,43 @@
 #include "lists.h"
+
 /**
- * add_dnodeint - Func adds a new node
- * @head: Head
- * @n: Elem. value
+ * add_dnodeint - Adds a new node at
+ * the beginning of a dlistint_t list
+ * @head: Pointer to the head of the list
+ * @n: Integer value for the new node
  *
- * Return: Elem. address
+ * Return: Address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *u, *v;
+	dlistint_t *new_node, *current_head;
 
-	u = malloc(sizeof(dlistint_t));
-	if (!u)
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
 		return (NULL);
-	u->n = n;
-	u->prev = NULL;
-	v = *head;
 
-	if (v != NULL)
+	/* Initialize the new node */
+	new_node->n = n;
+	new_node->prev = NULL;
+
+	/* Find the current head of the list */
+	current_head = *head;
+
+	/* Traverse to the first node if the list is not empty */
+	if (current_head != NULL)
 	{
-		while (v->prev != NULL)
-			v = v->prev;
+		while (current_head->prev != NULL)
+			current_head = current_head->prev;
 	}
-	u->next = v;
-	if (v != NULL)
-		v->prev = u;
-	*head = u;
-	return (u);
+
+	/* Update pointers for the new node and the current head */
+	new_node->next = current_head;
+	if (current_head != NULL)
+		current_head->prev = new_node;
+
+	/* Update the head to point to the new node */
+	*head = new_node;
+
+	return (new_node);
 }
